32e0f30b12b9936949c3ea39ba14b679ee5e7519
Kizi061 <64550503+Kizi061@users.noreply.github.com>
2020-11-10 11:39:24 -0600

    Update migrate_mule.bat
    Change the src and destination file [ath

--- mule-migration-assistant-runner-1.0.0/migrate_mule.bat
+++ mule-migration-assistant-runner-1.0.0/migrate_mule.bat
@@ -1,1 +1,1 @@
-java -jar mule-migration-assistant-runner-1.0.0.jar -muleVersion 4.3.0 -projectBasePath E:\AnypointStudioOldVer\workspace\mel-flow-example2 -destinationProjectBasePath E:\NJCMule_Program\mule3TOmule4\mel-flow-example2_mule4
+java -jar mule-migration-assistant-runner-1.0.0.jar -muleVersion 4.3.0 -projectBasePath \mule4-migration-db\sfdc_mule4_demo -destinationProjectBasePath \mule4-migration-db\sfdc_mule4_demo_mule4
